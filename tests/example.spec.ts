c0b4fbb1f76132f43391e6406952840acbe2f25e
Marvin Luchs <marvin.luchs@holisticon.de>
2023-12-04 21:05:58 +0100

    install playwright

---
+++ tests/example.spec.ts
@@ -0,0 +1,9 @@
+import { expect, test } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/New Remix App/);
+  await expect(page.getByRole('heading', { name: 'Welcome to Remix' })).toBeVisible();
+});
